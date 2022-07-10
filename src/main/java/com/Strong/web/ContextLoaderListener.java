1daf7474e41b3b870e0e830c8016268c7323b15b
Strong <21200100001@stu.xidian.edu.cn>
2022-07-10 14:32:54 +0800

    水

---
+++ src/main/java/com/Strong/web/ContextLoaderListener.java
@@ -0,0 +1,24 @@
+package com.Strong.web;
+
+import javax.servlet.ServletContextEvent;
+import javax.servlet.ServletContextListener;
+import javax.servlet.annotation.WebListener;
+
+@WebListener
+public class ContextLoaderListener implements ServletContextListener {
+
+    public ContextLoaderListener() {
+    }
+
+    @Override
+    public void contextInitialized(ServletContextEvent sce) {
+        /* This method is called when the servlet context is initialized(when the Web application is deployed). */
+    }
+
+    @Override
+    public void contextDestroyed(ServletContextEvent sce) {
+        /* This method is called when the servlet Context is undeployed or Application Server shuts down. */
+    }
+
+
+}
